a801b5ef288660e5f73cb98d5079024c12fb80bd
ShoelessJo3 <gojoericker@comcast.net>
2017-08-24 23:25:37 -0500

    test1

--- js/main.js
+++ js/main.js
@@ -1,104 +1,130 @@
 var game = new Phaser.Game(1280, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render});
-var image;
-function preload() {
 
-    //  You can fill the preloader with as many assets as your game requires
+function init(){
 
-    //  Here we are loading an image. The first parameter is the unique
-    //  string by which we'll identify the image later in our code.
-
-    //  The second parameter is the URL of the image (relative)
-    game.load.image('einstein', 'assets/pics/frog_on_uni.jpg');
-    game.load.atlasJSONHash('bot', 'assets/sprites/pole.png', 'assets/sprites/pole.json');
-
+	game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+	game.scale.pageAlignHorizontally = true;
+	game.scale.pageAlignVertically= true;
 }
 
+function preload() {
+ game.load.image('paddle', 'assets/sprites/betterpaddle.png');
+ game.load.image('ball', 'assets/sprites/ball.png');
+game.load.bitmapFont('carrier_command', 'assets/fonts/bitmapFonts/carrier_command.png', 'assets/fonts/bitmapFonts/carrier_command.xml');
+}
+var paddle1;
+var cursors;
+var customBounds;
+var font;
 function create() {
+	
+	var i = game.add.image(100,100,font)
+	var bounds = new Phaser.Rectangle(100, 100, 400, 400);
+	image = game.add.sprite(0, 0, 'paddle');
+	game.world.setBounds(0, 0, 1280, 600);
 
-    //  This creates a simple sprite that is using our loaded image and
-    //  displays it on-screen
-    //  and assign it to a variable
+    //  Enable P2 and it will use the updated world size
+    game.physics.startSystem(Phaser.Physics.P2JS);
 
-    image = game.add.sprite(0, 0, 'einstein');
-    game.physics.enable(image, Phaser.Physics.ARCADE);
-
-    image.body.velocity.x=150;
     
 
-    game.leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
-	game.rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
-	game.spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+	paddle1 = game.add.sprite(100, 10, 'paddle');
+    paddle1.scale.set(4);
+    paddle1.smoothed = false;
+    game.physics.p2.enable(paddle1, false);
+    paddle1.body.fixedRotation = true;
+    paddle1.body.damping = .5;
 
-    //  Stop the following keys from propagating up to the browser
-    game.input.keyboard.addKeyCapture([ Phaser.Keyboard.LEFT, Phaser.Keyboard.RIGHT, Phaser.Keyboard.SPACEBAR ]);
+    paddle2 = game.add.sprite(1180,10, 'paddle');
+    paddle2.scale.set(4);
+    paddle2.smoothed = false;
+    game.physics.p2.enable(paddle2, false);
+    paddle2.body.fixedRotation = true;
+    paddle2.body.damping = .5;
 
-    //  This sprite is using a texture atlas for all of its animation data
-    var bot = game.add.sprite(200, 200, 'bot');
+    var spriteMaterial = game.physics.p2.createMaterial('spriteMaterial', paddle1.body);
 
-    //  Here we add a new animation called 'run'
-    //  We haven't specified any frames because it's using every frame in the texture atlas
-    bot.animations.add('run',[0,1,2,3],15,true);
+    var worldMaterial = game.physics.p2.createMaterial('worldMaterial');
+
+    //  4 trues = the 4 faces of the world in left, right, top, bottom order
+    game.physics.p2.setWorldMaterial(worldMaterial, true, true, true, true);
+
+    //  Here is the contact material. It's a combination of 2 materials, so whenever shapes with
+    //  those 2 materials collide it uses the following settings.
+    //  A single material can be used by as many different sprites as you like.
+    var contactMaterial = game.physics.p2.createContactMaterial(spriteMaterial, worldMaterial);
+
+    contactMaterial.friction = 0.3;     // Friction to use in the contact of these two materials.
+    contactMaterial.restitution = 1.0;  // Restitution (i.e. how bouncy it is!) to use in the contact of these two materials.
+    contactMaterial.stiffness = 1e7;    // Stiffness of the resulting ContactEquation that this ContactMaterial generate.
+    contactMaterial.relaxation = 3;     // Relaxation of the resulting ContactEquation that this ContactMaterial generate.
+    contactMaterial.frictionStiffness = 1e7;    // Stiffness of the resulting FrictionEquation that this ContactMaterial generate.
+    contactMaterial.frictionRelaxation = 3;     // Relaxation of the resulting FrictionEquation that this ContactMaterial generate.
+    contactMaterial.surfaceVelocity = 0;        // Will add surface velocity to this material. If bodyA rests on top if bodyB, and the surface velocity is positive, bodyA will slide to the right.
 
 
-    //  And this starts the animation playing by using its key ("run")
-    //  15 is the frame rate (15fps)
-    //  true means it will loop when it finishes
-    bot.animations.play('run');
-    bot.scale.setTo(10,10);
 
+    //  Create our physics body. The 'true' parameter enables visual debugging.
+
+
+
+    //  Alternatively create a circle for the ship instead (which more accurately matches its size)
+    // ship.body.setCircle(28);
+
+
+
+    
+    
+
+    //game.physics.p2.gravity.y = 100;
+    //  Just to display the bounds
+
+
+    cursors = game.input.keyboard.createCursorKeys();
+    bmpText = game.add.bitmapText(270, 500, 'carrier_command', 'Player 1: Q&A \nPlayer 2: O&L', 46);
+    
+
+
+    
 }
+
+
 
 function update(){
 
-	if(game.leftKey.isDown)
-	{
-		image.body.velocity.x=-150;
-	}
-	if(game.rightKey.isDown)
-	{
-		image.body.velocity.x=150;
-	}
-	if(game.spaceKey.isDown)
-	{
-		image.body.velocity.x=0;
-	}
+	//paddle1.body.setZeroXVelocity();
+
+    if (game.input.keyboard.isDown(Phaser.Keyboard.Q))
+    {
+    	paddle1.body.moveUp(350);
+    }
+    else if (game.input.keyboard.isDown(Phaser.Keyboard.A))
+    {
+        paddle1.body.moveDown(350);
+    }
+    if(paddle1.body.x != 100)
+    {
+    	paddle1.body.x = 100;
+    }
+
+    if (game.input.keyboard.isDown(Phaser.Keyboard.O))
+    {
+    	paddle2.body.moveUp(350);
+    }
+    else if (game.input.keyboard.isDown(Phaser.Keyboard.L))
+    {
+        paddle2.body.moveDown(350);
+    }
+    if(paddle2.body.x != 1180)
+    {
+    	paddle2.body.x = 1180;
+    }
+
+
 }
 
 function render(){
 
-	var x = 32;
-    var y = 0;
-    var yi = 32;
 
-    game.debug.text('Viewport', x, y += yi);
-
-    game.debug.text('Viewport Width: ' + game.scale.viewportWidth, x, y += yi);
-    game.debug.text('window.innerWidth: ' + window.innerWidth, x, y += yi);
-    game.debug.text('window.outerWidth: ' + window.outerWidth, x, y += yi);
-
-    game.debug.text('Viewport Height: ' + game.scale.viewportHeight, x, y += yi);
-    game.debug.text('window.innerHeight: ' + window.innerHeight, x, y += yi);
-    game.debug.text('window.outerHeight: ' + window.outerHeight, x, y += yi);
-
-    game.debug.text('Document', x, y += yi*2);
-
-    game.debug.text('Document Width: ' + game.scale.documentWidth, x, y += yi);
-    game.debug.text('Document Height: ' + game.scale.documentHeight, x, y += yi);
-
-    //  Device: How to get device size.
-
-    //  Use window.screen.width for device width and window.screen.height for device height. 
-    //  .availWidth and .availHeight give you the device size minus UI taskbars. (Try on an iPhone.) 
-    //  Device size is static and does not change when the page is resized or rotated.
-
-    x = 350;
-    y = 0;
-
-    game.debug.text('Device', x, y += yi);
-
-    game.debug.text('window.screen.width: ' + window.screen.width, x, y += yi);
-    game.debug.text('window.screen.availWidth: ' + window.screen.availWidth, x, y += yi);
-    game.debug.text('window.screen.height: ' + window.screen.height, x, y += yi);
-    game.debug.text('window.screen.availHeight: ' + window.screen.availHeight, x, y += yi);
-
+    
 }
